8551446b07564844d7bb0536f1efbf8958300c21
KrafIT <krafittechnologies@gmail.com>
2024-09-22 10:14:06 +0530

    Initial commit

---
+++ src/main/java/com/krafit/financialcalcs/FinancialcalcsApplication.java
@@ -0,0 +1,13 @@
+package com.krafit.financialcalcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialcalcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialcalcsApplication.class, args);
+	}
+
+}
